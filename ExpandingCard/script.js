d3ce9571de6c2b9c44426ce30a193200f98e3879
Harshil Bhardwaj <harshilofficial4@gmail.com>
2023-03-16 07:14:59 +0530

    test

--- ExpandingCard/script.js
+++ ExpandingCard/script.js
@@ -7,8 +7,13 @@ panels.forEach((panel) => {
   });
 });
 
-function removeActiveClasses() {
-  panels.forEach((panel) => {
-    panel.classList.remove("active");
-  });
-}
+// This function removes the "active" class from all panels
+const removeActiveClasses = () => {
+  // `panels` is a collection of DOM elements, and we're using the
+  // `forEach()` method to iterate over each element in the collection
+
+  panels.forEach((panel) => panel.classList.remove("active"));
+  // The `panel` argument represents each element in the `panels`
+  // collection. We're calling the `remove()` method on each `panel`
+  // element to remove the "active" class.
+};
